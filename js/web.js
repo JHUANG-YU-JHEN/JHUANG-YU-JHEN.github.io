bffd782758683bd5b5d0faca8a5e9b095cf9b9b5
JHUANG-YU-JHEN <134849312+JHUANG-YU-JHEN@users.noreply.github.com>
2023-06-04 01:45:36 +0800

    Update web.js

--- js/web.js
+++ js/web.js
@@ -5,10 +5,27 @@ $('#scrollToTop').on('click', (e) => {
 })
 
 
-$(document).ready(function() {
-    $(".question-title").click(function() {
-        $(this).next(".question-content").toggle();
-        $(this).find(".remove").toggle();
-        $(this).find(".add").toggle();
-    });
+$('.qa-item').click(function(e) {
+    $(this).toggleClass('active');
+    $(this).find('.add-icon').toggleClass('d-none');
+    $(this).find('.remove-icon').toggleClass('d-block');
+    $(this).find('.collapse-content p').toggleClass('show');
+});
+
+var mySwiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true, // 設置為循環播放
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 0, // 輪播延遲時間（單位：毫秒）
+        disableOnInteraction: false // 用戶操作後是否停止自動輪播
+    },
+    lazy: {
+        loadPrevNext: true,
+    },
+    speed: 3500, // 滑動速度（單位：毫秒）
+});
+
+$(".navbar-btn").click(function() {
+    $(".navbar-collapse").toggleClass("show");
 });
